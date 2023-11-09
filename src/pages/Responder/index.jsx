c8cd8aabf6fc90bb0023b6e808b5544e886b059a
Felipe-Alves-Cerquiare <felipe.cerquiares.alves@gmail.com>
2023-11-09 11:00:34 -0300

    Tela de responder em forms

--- src/pages/Responder/index.jsx
+++ src/pages/Responder/index.jsx
@@ -77,30 +77,32 @@ export const Responder = () => {
       <div className='row justify-content-center' style={{paddingLeft:92, paddingRight:92, paddingTop:77, backgroundColor:'#EAF3FB'}}>
         <div className="card col-lg-8 col-md-7">
           <div className='card-body'>
-            <div style={{marginTop: "4px"}} className='mb-4'>
-              <h3 >Resolução da Questão</h3>
-            </div>
-            <div className='mb-3 text-end'>
-              <span className='Pontuacao' style={{fontSize:"18px"}}>Valor: {question? question.score/(2**attempts) : 'carregando'} pts</span>
-            </div>
-            <div className='mb-3'>
-              <span>{question? question.description : `carregando`}</span>
-            </div>
-            <div className="form-floating mb-3">
-              <input type="text" className="form-control" id="floatingInput" placeholder="Insira sua resposta"/>
-              <label className="TextIntern" htmlFor="floatingInput">Insira sua resposta</label>
-            </div>
-            <div className='mb-2'>
-              <div className='text-start mb-2'>
-                <button className="TextButton_Acordeon btn btn-primary py-1 px-3" type="submit" onClick={ answer } >Responder</button>
-                {/* sumir com esse botão e apresentar os textos abaixo?? */}
+            <form>
+              <div style={{marginTop: "4px"}} className='mb-4'>
+                <h3 >Resolução da Questão</h3>
               </div>
-              <div className='text-start'>
-                <span className='TextButton_Acordeon'>Sua resposta está correta</span>
-                <span className='TextButton_Acordeon' style={{color:'red'}}>Sua resposta está incorreta</span>
-                {/* fazer função para aparecer um ou outro */}
+              <div className='mb-3 text-end'>
+                <span className='Pontuacao' style={{fontSize:"18px"}}>Valor: {question? question.score/(2**attempts) : 'Carregando'} pts</span>
               </div>
-            </div>
+              <div className='mb-3'>
+                <span>{question? question.description : `Carregando`}</span>
+              </div>
+              <div className="form-floating mb-3">
+                <input type="text" className="form-control" id="floatingInput" placeholder="Insira sua resposta"/>
+                <label className="TextIntern" htmlFor="floatingInput">Insira sua resposta</label>
+              </div>
+              <div className='mb-2'>
+                <div className='text-start mb-2'>
+                  <button className="TextButton_Acordeon btn btn-primary py-1 px-3" type="submit" onClick={ answer } >Responder</button>
+                  {/* sumir com esse botão e apresentar os textos abaixo?? */}
+                </div>
+                <div className='text-start'>
+                  <span className='TextButton_Acordeon'>Sua resposta está correta</span>
+                  <span className='TextButton_Acordeon' style={{color:'red'}}>Sua resposta está incorreta</span>
+                  {/* fazer função para aparecer um ou outro */}
+                </div>
+              </div>
+            </form>
           </div>
         </div>
 
